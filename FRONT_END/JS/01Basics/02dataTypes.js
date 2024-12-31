c4f28ac622170fe816a04367b84b406f55a31f25
shivaprasada02@gmail.com <shivaprasada02@gmail.com>
2024-12-31 17:08:53 +0530

    datatypes

---
+++ FRONT_END/JS/01Basics/02dataTypes.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3) // we are using  nodejs , not browser
+
+//ecma script : Standard 
+
+let name = "shiv" // string
+let age =18      //number , range 2 to power 53, morethan=>bigint
+let isLogged = false // boolean
+null  //standalone value
+undefined //=> not assigned
+//symbol //=>unique
+
+//object
+
+console.log(typeof("dddhbhfb"))
+
+//typeof(null) => object
