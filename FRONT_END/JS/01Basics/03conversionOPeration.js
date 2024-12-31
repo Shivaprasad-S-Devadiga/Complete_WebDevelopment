043fe9287702e0d91d557cf5ef5ea0bfe214fa57
shivaprasada02@gmail.com <shivaprasada02@gmail.com>
2024-12-31 17:31:09 +0530

    conversion of daatatypes

---
+++ FRONT_END/JS/01Basics/03conversionOPeration.js
@@ -0,0 +1,19 @@
+ let score = null;
+
+ let valueInNumber = Number(score);
+ console.log(typeof(valueInNumber));
+ console.log(valueInNumber);
+
+ //if
+   // score==33 =>number
+   // score==33a =>string  =>NaN(not an number)
+   // score==null =>number =>0
+   // score==undefined =>undefined =>undifined
+
+let login = "";   
+let booleanlogin =Boolean(login);
+console.log(booleanlogin);
+
+let some =true;
+let stringValue = String(some);
+console.log(stringValue)
